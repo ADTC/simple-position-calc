67995ce92e11426237db5cdb24c105ea1dbf2715
Arundas TC <arundas.tc@gmail.com>
2024-12-29 15:32:12 +0800

    Fix validation problems in Text (number input)

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -12,34 +12,33 @@ export default function Text({
   value: number | undefined;
   onChange: (value: number | undefined) => void;
 }) {
-  const [stringValue, setStringValue] = useState<string>();
+  const [stringValue, setStringValue] = useState<string>("");
 
   useEffect(() => {
-    if (value !== undefined) setStringValue(value?.toString() ?? "");
+    if (value === undefined) setStringValue("");
+    else setStringValue(value?.toString() ?? "");
   }, [value]);
 
   useEffect(() => {
-    // Only allow digits and one decimal point anywhere in the string
-    if (!stringValue?.match(/^\d*\.?\d*$/)) {
-      onChange(undefined);
-    } else {
+    // Only allow optional hyphen at beginning, optional digits
+    // and one optional decimal point anywhere in the string
+    if (/^-?\d*\.?\d*$/.test(stringValue)) {
       const numberValue = parseFloat(stringValue);
-      if (!isNaN(numberValue)) {
-        onChange(numberValue);
-      } else {
-        onChange(undefined);
-      }
-    }
+
+      if (isNaN(numberValue)) onChange(undefined);
+      else onChange(numberValue);
+      //
+    } else onChange(undefined);
   }, [onChange, stringValue]);
 
   return (
     <TextField
-      error={!!stringValue && value === undefined}
       helperText={label}
       sx={xs ? { maxWidth: 130 } : undefined}
       variant="outlined"
       value={stringValue}
       onChange={(e) => setStringValue(e.target.value)}
+      onBlur={() => value === undefined && setStringValue("")}
     />
   );
 }
